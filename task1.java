80264188e6b8a4d8f22cf59f006c16d24d2e63ac
Ikram <115416397+Ikram001@users.noreply.github.com>
2022-10-09 21:31:56 +0500

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,13 @@
+class variables {
+public static void main(String[] args){
+int a=1;
+System.out.println( "INTEGER VARIABLE");
+System.out.println("here a variable store integer datatype "+a);
+double b=1.1;
+System.out.println( "FLOAT VARIABLE");
+System.out.println("here b variable store float datatype "+b);
+char c='A';
+System.out.println( "CHARACTER  VARIABLE");		
+System.out.println("here c variable store character datatype "+c);
+}
+}
