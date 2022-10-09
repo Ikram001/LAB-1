190c1ea7ad3c44bbec66c8788c36a0f3b29b9beb
Ikram <115416397+Ikram001@users.noreply.github.com>
2022-10-09 21:37:03 +0500

    Add files via upload

---
+++ task2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class evenorodd{
+public static void main(String[] args) {
+Scanner obj=new Scanner(System.in);
+int a;
+System.out.println("Enter a Number ");
+a=obj.nextInt();	
+if(a%2==0)
+System.out.println("number is even");
+else 
+{
+System.out.println("number is odd");
+}
+}
+}
