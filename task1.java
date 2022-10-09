e13a5283b2e56ade0e231ccc60a8a685a374eecb
Ikram <115416397+Ikram001@users.noreply.github.com>
2022-10-09 21:32:17 +0500

    Update task1.java

--- task1.java
+++ task1.java
@@ -3,7 +3,7 @@ public static void main(String[] args){
 int a=1;
 System.out.println( "INTEGER VARIABLE");
 System.out.println("here a variable store integer datatype "+a);
-double b=1.1;
+double b=0.1;
 System.out.println( "FLOAT VARIABLE");
 System.out.println("here b variable store float datatype "+b);
 char c='A';
